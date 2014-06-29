023af6c4292ea2379971d26a13cd73d1e345d3fe
kei10in <kei10in@gmail.com>
2014-06-29 10:51:17 +0900

    improve scale quality

--- src/main/scala/model/Avatar.scala
+++ src/main/scala/model/Avatar.scala
@@ -26,13 +26,18 @@ class FileAvatar(filepath: File) extends Avatar {
     val img = image()
     val scaledImg = new BufferedImage(s, s, img.getType)
     val g = scaledImg.createGraphics()
+
     g.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
     g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
     g.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
     g.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
-    g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-    g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
     g.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_NORMALIZE);
+
+    if (s < img.getWidth) {
+      g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+      g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+    }
+
     g.drawImage(img, 0, 0, s, s, null)
     scaledImg
   }
